83e565f9fffb5d6e2313a956d47ae58bbb5c8b1d
mohammadraziei <mohammadraziei1375@gmail.com>
2019-10-02 14:11:27 +0330

    ALG

---
+++ Thesis/Latex template 2018/code/action-translation.py
@@ -0,0 +1,21 @@
+def action_translate(self,action):
+	lanewidth = self.enviroment.road.laneWidth
+	self.__action_old__ = self.__action__
+	vel = self.agent['data']['Velocity']
+	offset = self.__action__[0]
+
+
+	if action == 0 :
+		offset = -lanewidth
+	if action == 1 :
+		offset = 0 
+	if action == 2 :
+		offset = lanewidth
+	if action == 3 :
+		vel = action_velocity(vel,True)
+	if action == 4 :
+		vel = action_velocity(vel,False)
+
+	self.__action__ = [offset,vel]
+
+	return self.__action__
